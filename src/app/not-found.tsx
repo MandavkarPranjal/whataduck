5b85deff8c63344714695cf8e60bd9feea692ec2
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-02-16 18:25:43 +0530

    Added 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+export default function NotFoundPage() {
+    return (
+        <div className="flex min-h-screen flex-col justify-between p-8 font-[family-name:var(--font-geist-sans)] sm:p-20">
+            <main className="flex flex-grow flex-col items-center justify-center text-center">
+                <h1 className="text-3xl">
+                    {"You're lost in the woods!"}
+                </h1>
+                {/* Add single horizontal line to the above heading */}
+                <div className="mt-4 h-px w-96 bg-gray-700/70 rounded-full"></div>
+                <div className="mt-4 text-xl text-gray-300">
+                    Try{" "}
+                    <a
+                        href="https://whataduckk.netlify.app/bang?q=!gpt%20Quote%20to%20motivate%20me"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="italic underline text-gray-400 hover:text-gray-200"
+                    >
+                        "!gpt Quote to motivate me"
+                    </a>
+                </div>
+            </main>
+            <footer className="mt-8 text-center text-sm text-gray-500">
+                <a
+                    href="https://github.com/MandavkarPranjal/quicklink"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                >
+                    View on GitHub
+                </a>
+            </footer>
+        </div>
+    );
+}
