cf642e96f04f14bfff1558bb49c4b20150e6b11c
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-02-23 00:24:19 +0530

    Now it should work on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+// Derive __dirname in an ES module context.
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-    }),
-  ],
+    plugins: [
+        VitePWA({
+            registerType: "autoUpdate",
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                search: resolve(__dirname, "search.html"),
+            },
+        },
+    },
 });
